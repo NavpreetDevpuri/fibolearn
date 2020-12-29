94af45a112ebea6f6c4bc583dddfbb75b439de23
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2020-12-29 21:06:33 +0530

    travis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// n = 365;
+// f = [0, 1];
+// fp = 1;
+// fi = 1;
+// while (fi <= n) {
+//     f.push(fi);
+//     temp = fi;
+//     fi = fp + fi;
+//     fp = temp;
+// }
+// var l = Array(n).fill(0);
+// for (i = 0; i < l.length; ++i) {
+//     for (j of f) {
+//         if (i + j <= n) {
+//             ++l[i + j];
+//         }
+//     }
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
