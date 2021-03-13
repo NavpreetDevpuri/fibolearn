6c0befc8aff7da3dee25e59ff3d1741b1b6b7791
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2021-03-13 13:43:13 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function App() {
+    return (
+        <div className="App">
+            <iframe
+                style={{
+                    height: "100%",
+                    position: "absolute",
+                    left: "0px",
+                    width: "100%",
+                    overflow: "hidden",
+                }}
+                src="https://docs.google.com/document/d/e/2PACX-1vRlvq9bwB5F9T2vEmEjIYaOtzR2XITLBWVF_HWj4UuhBr1haYXHMV3qI_Rl45rT-_aitqZsj2rzAVcr/pub?embedded=true"
+            ></iframe>
+        </div>
+    );
+}
+
+export default App;
+
 // n = 365;
-// f = [0, 1];
+// f = [0];
+// fit = [1, 1, 1, 1, 2, 2, 2, 3];
 // fp = 1;
 // fi = 1;
 // while (fi <= n) {
@@ -11,34 +31,25 @@ import "./App.css";
 //     fi = fp + fi;
 //     fp = temp;
 // }
-// var l = Array(n).fill(0);
-// for (i = 0; i < l.length; ++i) {
+// var c = 0,
+//     total = 0;
+// var l = Array(n)
+//     .fill(null)
+//     .map(() => []);
+// for (i = 0; i < l.length; i += fit[c]) {
+//     total++;
 //     for (j of f) {
-//         if (i + j <= n) {
-//             ++l[i + j];
+//         if (i + j < n) {
+//             if (l[i + j].length > 8) {
+//                 l[i + j].pop();
+//             }
+//             l[i + j].push(i);
 //         }
 //     }
+//     if (c == 7) {
+//         c = 0;
+//     } else {
+//         c++;
+//     }
 // }
-
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
-
-export default App;
+// console.log({ l: l, r: n / total });
